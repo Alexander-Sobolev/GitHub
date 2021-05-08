b1a478d98f5fc006a6174e5a236bc5d3de2c72b7
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-05-08 20:33:29 +0300

    Hello Swift !

--- GitHub/ViewController.swift
+++ GitHub/ViewController.swift
@@ -18,6 +18,6 @@ class ViewController: UIViewController {
     // Hello Swift ! Hello Swift !
     // Hello Swift ! Hello Swift !
     // Hello Swift ! Hello Swift !
-    // Hello Swift !
+    // Hello Swift ! Hello Swift !
 }
 
